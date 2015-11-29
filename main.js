77274acf44286a94dd9ac0e666f04af634f1fa70
Bernardo Loureiro <bernardo.lou@gmail.com>
2015-11-29 00:07:48 -0200

    -request +request-promise

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var fs      = require('fs');
 var yaml    = require('js-yaml');
 var express = require('express');
 var app     = express();
-var request = require('request');
+var request = require('request-promise');
 var url     = require('url');
 var _       = require('lodash');
 
@@ -46,7 +46,7 @@ var handleauth = function (req, res) {
   console.log('responsing handleauth...');
   var url_parts = url.parse(req.url, true);
   access_token = url_parts.query.code;
-  res.send(url_parts.query);
+  console.log('setting access_token:', access_token);
 
   if (!_.isUndefined(url_parts.query.error)) {
     console.log(url_parts.query);
@@ -61,36 +61,77 @@ app.get('/handleauth', handleauth);
 
 /**********************************************************************/
 
-var block_followers = function (req, res) {
+var followed_by = function (req, res) {
   console.log('requesting followed-by...');
+  console.log('using access_token:', access_token);
 
-  request('https://api.instagram.com/v1/users/self/followed-by?access_token=' + access_token, function (error, response, body) {
-      console.log('followed-by body: ', body);
+  var options = {
+    uri: 'https://api.instagram.com/v1/users/self/followed-by',
+    qs: {
+      client_id: config.instagram.client.id,
+      access_token: access_token
+    },
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json: true
+  };
 
-      if (!error && response.statusCode === 200) {
-        body = JSON.parse(body);
+  return request(options)
+    .then(function (response) {
+      return response;
+    })
+    .catch(function (response) {
+      console.log('followed-by response:',
+        response.name,
+        response.statusCode,
+        response.error,
+        response.statusMessage
+        // response.req._header,
+        // response.request.href
+      );
+      process.exit(1);
+    });
+};
 
-        _(body.data).forEach(function (n) {
-          console.log(n.id);
+var block_followers = function (req, res) {
+  followed_by(req, res)
+    .then(function (followers) {
+      console.log('iterating followers...', followers);
 
-          request.post({uri: 'https://api.instagram.com/v1/users/' + n.id + '/relationship?access_token=' + access_token, form: { action: 'unfollow' }}, function (error, response, body) {
+      _(followers).forEach(function (n) {
+        console.log('blocking id:', n.id);
 
-            console.log(response.statusCode, response.statusMessage, response.body);
+        var options = {
+          method: 'POST',
+          uri: 'https://api.instagram.com/v1/users/' + n.id + '/relationship',
+          qs: {
+            access_token: access_token
+          },
+          body: {
+            action: 'unfollow'
+          },
+          json: true // Automatically stringifies the body to JSON
+        };
 
-            if (!error && response.statusCode === 200) {
-              console.log('success: ', body);
-            }
-            else {
-              console.log('error: ', error);
-            }
+        request(options)
+          .then(function (body) {
+            console.log('relationship body:', body);
+          })
+          .catch(function (response) {
+            console.log('relationship response:',
+              response.name,
+              response.statusCode,
+              response.error,
+              response.statusMessage
+              // response.req._header,
+              // response.request.href
+            );
+            process.exit(1);
           });
-        }).value();
-      }
-      else {
-        console.log(error);
-        res.redirect('/authorize_user');
-      }
-  });
+
+      }).value();
+    });
 };
 
 app.get('/block_followers', block_followers);
